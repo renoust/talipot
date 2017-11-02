07ce18ec683c25421467167d4a2d8fe9ce289f57
Antoine Lambert <antoine.lambert@inria.fr>
2017-11-02 22:46:18 +0100

    experiment with uploading Tulip packages on Bintray

--- bundlers/linux/tulip_appimage_centos6_build.sh
+++ bundlers/linux/tulip_appimage_centos6_build.sh
@@ -33,7 +33,7 @@ yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel r
 wget http://python.org/ftp/python/2.7.14/Python-2.7.14.tar.xz
 tar xf Python-2.7.14.tar.xz
 cd Python-2.7.14
-./configure --prefix=/usr --enable-unicode=ucs4 --enable-shared --enable-optimizations
+./configure --prefix=/usr --enable-unicode=ucs4 --enable-shared --enable-optimizations CC="ccache gcc" CXX="ccache g++"
 make -j4 && make altinstall
 
 # some extra setup for cmake et appimage build to succeed
@@ -47,8 +47,9 @@ python2.7 get-pip.py
 pip2.7 install sphinx
 
 # build and install tulip
-cd && mkdir tulip_build && cd tulip_build
-cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python2.7 -DCMAKE_INSTALL_PREFIX=$PWD/install -DTULIP_USE_CCACHE=ON /tulip
+mkdir /tulip/build
+cd /tulip/build
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python2.7 -DCMAKE_INSTALL_PREFIX=$PWD/install -DTULIP_USE_CCACHE=ON ..
 make -j4 install
 
 # build a bundle dir suitable for AppImageKit
@@ -59,4 +60,4 @@ wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appi
 chmod a+x appimagetool-x86_64.AppImage
 
 # finally build the portable app
-./appimagetool-x86_64.AppImage Tulip.AppDir Tulip-$(install/bin/tulip-config --version)
+./appimagetool-x86_64.AppImage Tulip.AppDir Tulip-$(install/bin/tulip-config --version).AppImage
