renoust/talipot

commit f3727547eddf9e0bfd25defb33a4aaa0642c2510

Modernize the use and syntax of iterators using C++11
In order to improve code readability, remove the direct use of
STL iterators whenever it is possible with for range based loops
or use auto keyword in order to get rid of that awful verbose syntax
from C++98.

For consistency across the Talipot API, all methods taking Talipot
iterators as parameters take ownership of them.

Also integrate https://github.com/Tulip-Dev/tulip/commit/53eed2a5396b8f83007317140f6f31ebd585dde5
from upstream Tulip.

library/talipot-core/src/DoubleProperty.cpp
@@ -64,8 +64,7 @@ static void computeEdgeAvgValue(
   double value = 0;
   unsigned int nbEdges = 0;
 
-  while (itE->hasNext()) {
-    edge e = itE->next();
+  for (auto e : itE) {
     ++nbEdges;
     value += metric->getEdgeValue(e);
   }
@@ -101,8 +100,7 @@ static void computeEdgeSumValue(
     Iterator<edge> *itE) {
   double value = 0;
 
-  while (itE->hasNext()) {
-    edge e = itE->next();
+  for (auto e : itE) {
     value += metric->getEdgeValue(e);
   }
 
@@ -139,8 +137,8 @@ static void computeEdgeMaxValue(
     Iterator<edge> *itE) {
   double value = -DBL_MAX;
 
-  while (itE->hasNext()) {
-    const double &eVal = metric->getEdgeValue(itE->next());
+  for (auto e : itE) {
+    const double &eVal = metric->getEdgeValue(e);
 
     if (eVal > value)
       value = eVal;
@@ -179,8 +177,8 @@ static void computeEdgeMinValue(
     Iterator<edge> *itE) {
   double value = DBL_MAX;
 
-  while (itE->hasNext()) {
-    const double &eVal = metric->getEdgeValue(itE->next());
+  for (auto e : itE) {
+    const double &eVal = metric->getEdgeValue(e);
 
     if (eVal < value)
       value = eVal;

plugins/clustering/StrengthClustering.cpp
@@ -30,13 +30,11 @@ double StrengthClustering::computeMQValue(const vector<unordered_set<node>> &par
   map<pair<unsigned int, unsigned int>, unsigned int> nbExtraEdges;
 
   MutableContainer<unsigned int> clusterId;
-  auto itPart = partition.begin();
 
-  for (unsigned int i = 0; itPart != partition.end(); ++itPart, ++i) {
-    auto itSet = itPart->begin();
-
-    for (; itSet != itPart->end(); ++itSet) {
-      clusterId.set(itSet->id, i);
+  unsigned int i = 0;
+  for (const auto &p : partition) {
+    for (auto n : p) {
+      clusterId.set(n.id, i++);
     }
   }
 
@@ -77,11 +75,10 @@ double StrengthClustering::computeMQValue(const vector<unordered_set<node>> &par
   positive /= double(partition.size());
 
   double negative = 0;
-  map<pair<unsigned int, unsigned int>, unsigned int>::const_iterator itMap = nbExtraEdges.begin();
 
-  for (; itMap != nbExtraEdges.end(); ++itMap) {
-    const pair<unsigned int, unsigned int> &pp = itMap->first;
-    unsigned int val = itMap->second;
+  for (const auto &itMap : nbExtraEdges) {
+    const auto &pp = itMap.first;
+    unsigned int val = itMap.second;
 
     if (!partition[pp.first].empty() && !partition[pp.second].empty())
       negative += double(val) / double(partition[pp.first].size() * partition[pp.second].size());
@@ -251,10 +248,8 @@ bool StrengthClustering::run() {
   computeNodePartition(threshold, tmp);
 
   for (unsigned int i = 0; i < tmp.size(); ++i) {
-    unordered_set<node>::const_iterator it;
-
-    for (it = tmp[i].begin(); it != tmp[i].end(); ++it) {
-      result->setNodeValue(*it, i);
+    for (auto n : tmp[i]) {
+      result->setNodeValue(n, i);
     }
   }
 

plugins/layout/GEMLayout.cpp
@@ -98,13 +98,13 @@ void GEMLayout::vertexdata_init(const float starttemp) {
   _center.fill(0);
   std::vector<GEMparticule>::iterator it;
 
-  for (it = _particules.begin(); it != _particules.end(); ++it) {
-    it->heat = starttemp;
-    _temperature += it->heat * it->heat;
-    it->imp.fill(0);
-    it->dir = 0;
-    it->mass = 1.f + it->mass / 3.f;
-    _center += it->pos;
+  for (auto &p : _particules) {
+    p.heat = starttemp;
+    _temperature += p.heat * p.heat;
+    p.imp.fill(0);
+    p.dir = 0;
+    p.mass = 1.f + p.mass / 3.f;
+    _center += p.pos;
   }
 }
 //=========================================================

plugins/layout/HierarchicalGraph.cpp
@@ -75,10 +75,7 @@ inline unsigned int HierarchicalGraph::degree(tlp::Graph *sg, tlp::node n, bool 
 }
 //================================================================================
 void HierarchicalGraph::twoLayerCrossReduction(tlp::Graph *sg, unsigned int freeLayer) {
-  vector<node>::const_iterator it;
-
-  for (it = grid[freeLayer].begin(); it != grid[freeLayer].end(); ++it) {
-    node n = *it;
+  for (auto n : grid[freeLayer]) {
     double sum = embedding->getNodeValue(n);
     unsigned int deg = 1;
     for (auto itn : sg->getInOutNodes(n)) {
@@ -165,11 +162,12 @@ void HierarchicalGraph::DagLevelSpanningTree(tlp::Graph *sg, tlp::DoubleProperty
         tmpVect.push_back(e);
       sort(tmpVect.begin(), tmpVect.end(), tmpL);
       int toKeep = tmpVect.size() / 2;
-      vector<edge>::const_iterator it;
 
-      for (it = tmpVect.begin(); it != tmpVect.end(); ++it, --toKeep) {
-        if (toKeep != 0)
-          sg->delEdge(*it);
+      for (auto e : tmpVect) {
+        if (toKeep != 0) {
+          sg->delEdge(e);
+        }
+        --toKeep;
       }
     }
   }
@@ -186,10 +184,9 @@ void HierarchicalGraph::computeEdgeBends(
   for (auto e : reversedEdges)
     isReversed.set(e.id, true);
 
-  for (std::unordered_map<edge, edge>::const_iterator it = replacedEdges.begin();
-       it != replacedEdges.end(); ++it) {
-    edge toUpdate = (*it).first;
-    edge start = (*it).second;
+  for (const auto &it : replacedEdges) {
+    edge toUpdate = it.first;
+    edge start = it.second;
     edge end = start;
     node tgt;
 
@@ -241,20 +238,22 @@ void HierarchicalGraph::computeSelfLoops(tlp::Graph *mySGraph, tlp::LayoutProper
     const LineType::RealType &edge1 = tmpLayout.getEdgeValue(tmp.e1);
     const LineType::RealType &edge2 = tmpLayout.getEdgeValue(tmp.e2);
     const LineType::RealType &edge3 = tmpLayout.getEdgeValue(tmp.e3);
-    LineType::RealType::const_iterator it;
 
-    for (it = edge1.begin(); it != edge1.end(); ++it)
-      tmpLCoord.push_back(*it);
+    for (const auto &c : edge1) {
+      tmpLCoord.push_back(c);
+    }
 
     tmpLCoord.push_back(tmpLayout.getNodeValue(tmp.n1));
 
-    for (it = edge2.begin(); it != edge2.end(); ++it)
-      tmpLCoord.push_back(*it);
+    for (const auto &c : edge2) {
+      tmpLCoord.push_back(c);
+    }
 
     tmpLCoord.push_back(tmpLayout.getNodeValue(tmp.n2));
 
-    for (it = edge3.begin(); it != edge3.end(); ++it)
-      tmpLCoord.push_back(*it);
+    for (const auto &c : edge3) {
+      tmpLCoord.push_back(c);
+    }
 
     result->setEdgeValue(tmp.old, tmpLCoord);
     mySGraph->delNode(tmp.n1, true);
@@ -468,8 +467,8 @@ bool HierarchicalGraph::run() {
       LineType::RealType tmp2;
       LineType::RealType::iterator it;
 
-      for (it = tmp.begin(); it != tmp.end(); ++it) {
-        tmp2.push_back(Coord(-(*it)[1], (*it)[0], (*it)[2]));
+      for (const auto &p : tmp) {
+        tmp2.push_back(Coord(-p[1], p[0], p[2]));
       }
 
       result->setEdgeValue(e, tmp2);

plugins/view/ParallelCoordinatesView/src/NominalParallelAxis.cpp
@@ -82,12 +82,12 @@ const set<unsigned int> &NominalParallelAxis::getDataInSlidersRange() {
   map<string, unsigned int> labelsInRange;
   vector<string>::iterator it;
 
-  for (it = labelsOrder.begin(); it != labelsOrder.end(); ++it) {
-    Coord labelCoord = glNominativeAxis->getAxisPointCoordForValue(*it);
+  for (const auto &l : labelsOrder) {
+    Coord labelCoord = glNominativeAxis->getAxisPointCoordForValue(l);
 
     if (labelCoord.getY() >= bottomSliderCoord.getY() &&
         labelCoord.getY() <= topSliderCoord.getY()) {
-      labelsInRange[*it] = 1;
+      labelsInRange[l] = 1;
     }
   }
 
@@ -106,12 +106,11 @@ const set<unsigned int> &NominalParallelAxis::getDataInSlidersRange() {
 void NominalParallelAxis::updateSlidersWithDataSubset(const set<unsigned int> &dataSubset) {
   float rotAngleBak = rotationAngle;
   rotationAngle = 0.0f;
-  set<unsigned int>::iterator it;
   Coord max(getBaseCoord());
   Coord min(getBaseCoord() + Coord(0.0f, getAxisHeight()));
 
-  for (it = dataSubset.begin(); it != dataSubset.end(); ++it) {
-    Coord labelCoord(getPointCoordOnAxisForData(*it));
+  for (auto d : dataSubset) {
+    Coord labelCoord(getPointCoordOnAxisForData(d));
 
     if (labelCoord.getY() < min.getY()) {
       min = labelCoord;

commit 865b0ac81805e3d81738f1765d7f2fb4ba4d2e9c

Fix a couple of warnings and memory leaks introduced in f372754

library/talipot-core/include/talipot/AbstractProperty.h
@@ -526,10 +526,13 @@ public:
   // mE is the meta edge, itE is an iterator on the underlying edges
   // mg is the graph owning mE
   void computeMetaValue(edge e, tlp::Iterator<edge> *itE, Graph *mg) override {
-    if (Tprop::metaValueCalculator)
+    if (Tprop::metaValueCalculator) {
       static_cast<typename tlp::AbstractProperty<Tnode, Tedge, Tprop>::MetaValueCalculator *>(
           Tprop::metaValueCalculator)
           ->computeMetaValue(this, e, itE, mg);
+    } else {
+      delete itE;
+    }
   }
   void setMetaValueCalculator(PropertyInterface::MetaValueCalculator *mvCalc) override {
     if (mvCalc &&
@@ -566,7 +569,9 @@ public:
     // to the underlying edges given by the iterator itE.
     // The method do not have to delete the iterator
     virtual void computeMetaValue(AbstractProperty<Tnode, Tedge, Tprop> *, edge,
-                                  tlp::Iterator<edge> *, Graph *) {}
+                                  tlp::Iterator<edge> *itE, Graph *) {
+      delete itE;
+    }
   };
 
 protected:

library/talipot-core/src/DoubleProperty.cpp
@@ -218,8 +218,11 @@ public:
   void
   computeMetaValue(AbstractProperty<tlp::DoubleType, tlp::DoubleType, tlp::NumericProperty> *metric,
                    edge mE, Iterator<edge> *itE, Graph *) override {
-    if (edgeCalc)
+    if (edgeCalc) {
       edgeCalc(metric, mE, itE);
+    } else {
+      delete itE;
+    }
   }
 };
 

plugins/clustering/StrengthClustering.cpp
@@ -34,8 +34,9 @@ double StrengthClustering::computeMQValue(const vector<unordered_set<node>> &par
   unsigned int i = 0;
   for (const auto &p : partition) {
     for (auto n : p) {
-      clusterId.set(n.id, i++);
+      clusterId.set(n.id, i);
     }
+    ++i;
   }
 
   for (auto e : sg->edges()) {

plugins/layout/GEMLayout.cpp
@@ -96,7 +96,6 @@ unsigned int GEMLayout::select() {
 void GEMLayout::vertexdata_init(const float starttemp) {
   _temperature = 0;
   _center.fill(0);
-  std::vector<GEMparticule>::iterator it;
 
   for (auto &p : _particules) {
     p.heat = starttemp;

plugins/layout/HierarchicalGraph.cpp
@@ -463,9 +463,8 @@ bool HierarchicalGraph::run() {
       result->setNodeValue(n, Coord(-tmpC[1], tmpC[0], tmpC[2]));
     }
     for (auto e : graph->edges()) {
-      LineType::RealType tmp = result->getEdgeValue(e);
+      const LineType::RealType &tmp = result->getEdgeValue(e);
       LineType::RealType tmp2;
-      LineType::RealType::iterator it;
 
       for (const auto &p : tmp) {
         tmp2.push_back(Coord(-p[1], p[0], p[2]));

plugins/view/ParallelCoordinatesView/src/NominalParallelAxis.cpp
@@ -80,7 +80,6 @@ const set<unsigned int> &NominalParallelAxis::getDataInSlidersRange() {
 
   dataSubset.clear();
   map<string, unsigned int> labelsInRange;
-  vector<string>::iterator it;
 
   for (const auto &l : labelsOrder) {
     Coord labelCoord = glNominativeAxis->getAxisPointCoordForValue(l);