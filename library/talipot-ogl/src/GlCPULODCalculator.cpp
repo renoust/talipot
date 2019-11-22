renoust/talipot

commit 92de3cdb9a87f0a566583aa410e85f216e4f9841

Remove calls to std::vector::emplace_back and std::*map::emplace
Calling this new methods from C++11 impacts code readability for negligible
performance gains (especially in Release mode where compilers will optimize
those instructions), so remove their use.

library/talipot-ogl/src/GlCPULODCalculator.cpp
@@ -33,9 +33,7 @@ GlCPULODCalculator::~GlCPULODCalculator() {}
 
 void GlCPULODCalculator::beginNewCamera(Camera *camera) {
   // add a new layerLODUnit and set camera pointer
-  layersLODVector.emplace_back();
-  currentLayerLODUnit = &layersLODVector.back();
-  currentLayerLODUnit->camera = camera;
+  layersLODVector.push_back(LayerLODUnit(camera));
 }
 
 void GlCPULODCalculator::addSimpleEntityBoundingBox(GlSimpleEntity *entity, const BoundingBox &bb) {
@@ -53,7 +51,7 @@ void GlCPULODCalculator::addSimpleEntityBoundingBox(GlSimpleEntity *entity, cons
     noBBCheck[ti] = true;
   }
 
-  currentLayerLODUnit->simpleEntitiesLODVector.emplace_back(entity, bb);
+  currentLayerLODUnit->simpleEntitiesLODVector.push_back(SimpleEntityLODUnit(entity, bb));
 }
 void GlCPULODCalculator::addNodeBoundingBox(unsigned int id, unsigned int pos,
                                             const BoundingBox &bb) {

library/talipot-core/src/DataSet.cpp
@@ -60,7 +60,7 @@ DataSet &DataSet::operator=(const DataSet &set) {
   if (this != &set) {
     data.clear();
     for (const auto &it : set.data) {
-      data.emplace(it.first, it.second->clone());
+      data[it.first] = it.second->clone();
     }
   }
   return *this;
@@ -116,7 +116,7 @@ void DataSet::setData(const std::string &str, const DataType *value) {
     it->second = val;
     return;
   }
-  data.emplace(str, val);
+  data[str] = val;
 }
 
 unsigned int DataSet::size() const {
@@ -209,7 +209,7 @@ bool DataSet::readData(std::istream &is, const std::string &prop,
     }
 
     // no preexisting value
-    data.emplace(prop, dt);
+    data[prop] = dt;
     return true;
   }
 

library/talipot-core/src/FontAwesome.cpp
@@ -58,7 +58,7 @@ const vector<string> &FontAwesome::getSupportedIcons() {
   if (iconsNames.empty()) {
     iconsNames.reserve(iconCodePoint.size());
     for (auto &it : iconCodePoint) {
-      iconsNames.emplace_back(it.first);
+      iconsNames.push_back(it.first);
     }
   }
   return iconsNames;

library/talipot-core/src/GraphUpdatesRecorder.cpp
@@ -202,13 +202,13 @@ void GraphUpdatesRecorder::deleteDefaultValues(
 void GraphUpdatesRecorder::recordEdgeContainer(
     std::unordered_map<node, std::vector<edge>> &containers, GraphImpl *g, node n, edge e) {
   if (containers.find(n) == containers.end()) {
-    auto itAdj = containers.emplace(std::make_pair(n, g->storage.adj(n))).first;
+    containers[n] = g->storage.adj(n);
     // if we got a valid edge, this means that we must record
     // the node adjacencies before that edge was added (see addEdge)
     if (e.isValid()) {
       // as the edge is the last added
       // it must be at the last adj position
-      auto &adj = itAdj->second;
+      auto &adj = containers[n];
       auto size = adj.size() - 1;
       assert(e == adj[size]);
       adj.resize(size);
@@ -220,7 +220,7 @@ void GraphUpdatesRecorder::recordEdgeContainer(
     std::unordered_map<node, std::vector<edge>> &containers, GraphImpl *g, node n,
     const vector<edge> &gEdges, unsigned int nbAdded) {
   if (containers.find(n) == containers.end()) {
-    auto &adj = containers.emplace(std::make_pair(n, g->storage.adj(n))).first->second;
+    auto &adj = containers[n] = g->storage.adj(n);
     // we must ensure that the last edges added in gEdges
     // are previously removed from the current node adjacencies,
     // so we look (in reverse order because they must be at the end)
@@ -295,7 +295,7 @@ void GraphUpdatesRecorder::recordNewValues(GraphImpl *g) {
     // loop on oldNodeDefaultValues
     for (const auto &itdv : oldNodeDefaultValues) {
       PropertyInterface *p = itdv.first;
-      newNodeDefaultValues.emplace(make_pair(p, p->getNodeDefaultDataMemValue()));
+      newNodeDefaultValues[p] = p->getNodeDefaultDataMemValue();
       recordNewNodeValues(p);
     }
 
@@ -336,7 +336,7 @@ void GraphUpdatesRecorder::recordNewValues(GraphImpl *g) {
 
       if (created) {
         if (hasNewValues)
-          newValues.emplace(make_pair(p, RecordedValues(nv, rn)));
+          newValues[p] = RecordedValues(nv, rn);
         else {
           delete nv;
           delete rn;
@@ -347,7 +347,7 @@ void GraphUpdatesRecorder::recordNewValues(GraphImpl *g) {
     // loop on oldEdgeDefaultValues
     for (const auto &itdv : oldEdgeDefaultValues) {
       PropertyInterface *p = itdv.first;
-      newEdgeDefaultValues.emplace(make_pair(p, p->getEdgeDefaultDataMemValue()));
+      newEdgeDefaultValues[p] = p->getEdgeDefaultDataMemValue();
       recordNewEdgeValues(p);
     }
 
@@ -388,7 +388,7 @@ void GraphUpdatesRecorder::recordNewValues(GraphImpl *g) {
 
       if (created) {
         if (hasNewValues)
-          newValues.emplace(make_pair(p, RecordedValues(nv, nullptr, re)));
+          newValues[p] = RecordedValues(nv, nullptr, re);
         else {
           delete nv;
           delete re;
@@ -451,7 +451,7 @@ void GraphUpdatesRecorder::recordNewNodeValues(PropertyInterface *p) {
 
   if (hasNewValues) {
     if (itnv == newValues.end())
-      newValues.emplace(make_pair(p, RecordedValues(nv, rn)));
+      newValues[p] = RecordedValues(nv, rn);
     else
       itnv->second.recordedNodes = rn;
   } else {
@@ -500,7 +500,7 @@ void GraphUpdatesRecorder::recordNewEdgeValues(PropertyInterface *p) {
 
   if (hasNewValues) {
     if (itnv == newValues.end())
-      newValues.emplace(make_pair(p, RecordedValues(nv, nullptr, re)));
+      newValues[p] = RecordedValues(nv, nullptr, re);
     else
       itnv->second.recordedEdges = re;
   } else {
@@ -935,8 +935,7 @@ void GraphUpdatesRecorder::addNode(Graph *g, node n) {
   auto itgn = graphAddedNodes.find(g);
 
   if (itgn == graphAddedNodes.end()) {
-    auto nl = {n};
-    graphAddedNodes.emplace(std::make_pair(g, unordered_set<node>(nl)));
+    graphAddedNodes[g] = {n};
   } else
     itgn->second.insert(n);
 
@@ -956,14 +955,13 @@ void GraphUpdatesRecorder::addEdge(Graph *g, edge e) {
   auto itge = graphAddedEdges.find(g);
 
   if (itge == graphAddedEdges.end()) {
-    auto el = {e};
-    graphAddedEdges.emplace(std::make_pair(g, unordered_set<edge>(el)));
+    graphAddedEdges[g] = {e};
   } else
     itge->second.insert(e);
 
   if (g == g->getRoot()) {
     auto eEnds = g->ends(e);
-    addedEdgesEnds.emplace(std::make_pair(e, eEnds));
+    addedEdgesEnds[e] = eEnds;
     // record source & target old adjacencies
     recordEdgeContainer(oldContainers, static_cast<GraphImpl *>(g), eEnds.first, e);
     recordEdgeContainer(oldContainers, static_cast<GraphImpl *>(g), eEnds.second, e);
@@ -978,13 +976,11 @@ void GraphUpdatesRecorder::addEdge(Graph *g, edge e) {
 }
 
 void GraphUpdatesRecorder::addEdges(Graph *g, unsigned int nbAdded) {
-  auto itge = graphAddedEdges.find(g);
-
-  if (itge == graphAddedEdges.end()) {
-    itge = graphAddedEdges.emplace(std::make_pair(g, unordered_set<edge>())).first;
+  if (graphAddedEdges.find(g) == graphAddedEdges.end()) {
+    graphAddedEdges[g] = {};
   }
 
-  unordered_set<edge> &ge = itge->second;
+  unordered_set<edge> &ge = graphAddedEdges[g];
   auto gEdges = g->edges();
 
   for (unsigned int i = gEdges.size() - nbAdded; i < gEdges.size(); ++i) {
@@ -993,7 +989,7 @@ void GraphUpdatesRecorder::addEdges(Graph *g, unsigned int nbAdded) {
 
     if (g == g->getRoot()) {
       auto eEnds = g->ends(e);
-      addedEdgesEnds.emplace(std::make_pair(e, eEnds));
+      addedEdgesEnds[e] = eEnds;
       // record source & target old adjacencies
       recordEdgeContainer(oldContainers, static_cast<GraphImpl *>(g), eEnds.first, gEdges, nbAdded);
       recordEdgeContainer(oldContainers, static_cast<GraphImpl *>(g), eEnds.second, gEdges,
@@ -1028,8 +1024,7 @@ void GraphUpdatesRecorder::delNode(Graph *g, node n) {
   itgn = graphDeletedNodes.find(g);
 
   if (itgn == graphDeletedNodes.end()) {
-    auto nl = {n};
-    graphDeletedNodes.emplace(std::make_pair(g, unordered_set<node>(nl)));
+    graphDeletedNodes[g] = {n};
   } else
     itgn->second.insert(n);
 
@@ -1086,8 +1081,7 @@ void GraphUpdatesRecorder::delEdge(Graph *g, edge e) {
   itge = graphDeletedEdges.find(g);
 
   if (itge == graphDeletedEdges.end()) {
-    auto el = {e};
-    graphDeletedEdges.emplace(std::make_pair(g, unordered_set<edge>(el)));
+    graphDeletedEdges[g] = {e};
   } else
     itge->second.insert(e);
 
@@ -1099,16 +1093,15 @@ void GraphUpdatesRecorder::delEdge(Graph *g, edge e) {
 
       if (it != revertedEdges.end()) {
         revertedEdges.erase(it);
-        deletedEdgesEnds.emplace(std::make_pair(e, std::make_pair(eEnds.second, eEnds.first)));
+        deletedEdgesEnds[e] = make_pair(eEnds.second, eEnds.first);
       } else {
 
         const auto ite = oldEdgesEnds.find(e);
 
         if (ite == oldEdgesEnds.end())
-          deletedEdgesEnds.emplace(std::make_pair(e, eEnds));
+          deletedEdgesEnds[e] = eEnds;
         else {
-          deletedEdgesEnds.emplace(
-              std::make_pair(e, std::make_pair(ite->second.first, ite->second.second)));
+          deletedEdgesEnds[e] = make_pair(ite->second.first, ite->second.second);
           // remove from oldEdgesEnds
           oldEdgesEnds.erase(ite);
           // remove from newEdgesEnds
@@ -1116,7 +1109,7 @@ void GraphUpdatesRecorder::delEdge(Graph *g, edge e) {
         }
       }
     } else
-      deletedEdgesEnds.emplace(std::make_pair(e, eEnds));
+      deletedEdgesEnds[e] = eEnds;
   }
 
   // get the set of added properties if any
@@ -1218,7 +1211,7 @@ void GraphUpdatesRecorder::afterSetEnds(Graph *g, edge e) {
 
 void GraphUpdatesRecorder::addSubGraph(Graph *g, Graph *sg) {
   // last added subgraph will be deleted first during undo/redo
-  addedSubGraphs.push_front(std::make_pair(g, sg));
+  addedSubGraphs.push_front({g, sg});
 
   // sg may already have nodes and edges
   // cf addCloneSubGraph
@@ -1238,7 +1231,7 @@ void GraphUpdatesRecorder::addSubGraph(Graph *g, Graph *sg) {
 
 void GraphUpdatesRecorder::delSubGraph(Graph *g, Graph *sg) {
 
-  std::pair<Graph *, Graph *> p = std::make_pair(g, sg);
+  std::pair<Graph *, Graph *> p = {g, sg};
 
   auto it = std::find(addedSubGraphs.begin(), addedSubGraphs.end(), p);
 
@@ -1271,7 +1264,7 @@ void GraphUpdatesRecorder::delSubGraph(Graph *g, Graph *sg) {
 
 void GraphUpdatesRecorder::removeGraphData(Graph *g) {
   for (Graph *sg : g->subGraphs()) {
-    std::pair<Graph *, Graph *> p = std::make_pair(g, sg);
+    std::pair<Graph *, Graph *> p = {g, sg};
     auto it = std::find(addedSubGraphs.begin(), addedSubGraphs.end(), p);
 
     if (it != addedSubGraphs.end()) {
@@ -1294,7 +1287,7 @@ void GraphUpdatesRecorder::addLocalProperty(Graph *g, const string &name) {
   PropertyInterface *prop = g->getProperty(name);
 
   if (it == addedProperties.end())
-    addedProperties.emplace(make_pair(g, set<PropertyInterface *>({prop})));
+    addedProperties[g] = {prop};
   else
     it->second.insert(prop);
 }
@@ -1325,7 +1318,7 @@ void GraphUpdatesRecorder::delLocalProperty(Graph *g, const string &name) {
   it = deletedProperties.find(g);
 
   if (it == deletedProperties.end())
-    deletedProperties.emplace(make_pair(g, set<PropertyInterface *>({prop})));
+    deletedProperties[g] = {prop};
   else
     it->second.insert(prop);
 
@@ -1341,7 +1334,7 @@ void GraphUpdatesRecorder::propertyRenamed(PropertyInterface *prop) {
     return;
   } else {
     if (renamedProperties.find(prop) == renamedProperties.end())
-      renamedProperties.emplace(std::make_pair(prop, prop->getName()));
+      renamedProperties[prop] = prop->getName();
   }
 }
 
@@ -1372,7 +1365,7 @@ void GraphUpdatesRecorder::beforeSetNodeValue(PropertyInterface *p, node n) {
 
       pv->copy(n, n, p);
       rn->set(n, true);
-      oldValues.emplace(make_pair(p, RecordedValues(pv, rn)));
+      oldValues[p] = RecordedValues(pv, rn);
     }
     // check for a previously recorded old value
     else {
@@ -1395,7 +1388,7 @@ void GraphUpdatesRecorder::beforeSetAllNodeValue(PropertyInterface *p) {
       beforeSetNodeValue(p, n);
     // then record the old default value
     // because beforeSetNodeValue does nothing if it has already been changed
-    oldNodeDefaultValues.emplace(make_pair(p, p->getNodeDefaultDataMemValue()));
+    oldNodeDefaultValues[p] = p->getNodeDefaultDataMemValue();
   }
 }
 
@@ -1426,7 +1419,7 @@ void GraphUpdatesRecorder::beforeSetEdgeValue(PropertyInterface *p, edge e) {
 
       pv->copy(e, e, p);
       re->set(e, true);
-      oldValues.emplace(make_pair(p, RecordedValues(pv, nullptr, re)));
+      oldValues[p] = RecordedValues(pv, nullptr, re);
     }
     // check for a previously recorded old value
     else {
@@ -1449,7 +1442,7 @@ void GraphUpdatesRecorder::beforeSetAllEdgeValue(PropertyInterface *p) {
       beforeSetEdgeValue(p, e);
     // then record the old default value
     // because beforeSetEdgeValue does nothing if it has already been changed
-    oldEdgeDefaultValues.emplace(make_pair(p, p->getEdgeDefaultDataMemValue()));
+    oldEdgeDefaultValues[p] = p->getEdgeDefaultDataMemValue();
   }
 }
 

library/talipot-core/src/MaterialDesignIcons.cpp
@@ -52,7 +52,7 @@ const vector<string> &MaterialDesignIcons::getSupportedIcons() {
   if (iconsNames.empty()) {
     iconsNames.reserve(iconCodePoint.size());
     for (auto &it : iconCodePoint) {
-      iconsNames.emplace_back(it.first);
+      iconsNames.push_back(it.first);
     }
   }
   return iconsNames;

library/talipot-gui/include/talipot/CSVGraphImport.h
@@ -78,7 +78,7 @@ public:
   };
 
   void addException(const std::string &value, Action action) {
-    _exceptions.emplace_back(value, action);
+    _exceptions.push_back(Exception(value, action));
   }
 
   void clearExceptions() {

library/talipot-gui/src/CaptionItem.cpp
@@ -191,8 +191,8 @@ void CaptionItem::generateSizeCaption(CaptionType captionType) {
 
   if (!_metricProperty) {
     vector<pair<double, float>> metricToSizeFiltered;
-    metricToSizeFiltered.emplace_back(0., 1.);
-    metricToSizeFiltered.emplace_back(1., 1.);
+    metricToSizeFiltered.push_back(make_pair(0., 1.));
+    metricToSizeFiltered.push_back(make_pair(1., 1.));
     _captionGraphicsItem->generateSizeCaption(metricToSizeFiltered, "empty", 0., 1.);
     return;
   }
@@ -228,14 +228,14 @@ void CaptionItem::generateSizeCaption(CaptionType captionType) {
 
   for (const auto &it : metricToSizeMap) {
     if (it.first >= nextValue) {
-      metricToSizeFiltered.emplace_back(it.first, it.second / maxSize);
+      metricToSizeFiltered.push_back({it.first, it.second / maxSize});
       nextValue += intervale;
     }
   }
 
   if (metricToSizeFiltered.empty()) {
-    metricToSizeFiltered.emplace_back(minProp, 0);
-    metricToSizeFiltered.emplace_back(maxProp, 0);
+    metricToSizeFiltered.push_back({minProp, 0});
+    metricToSizeFiltered.push_back({maxProp, 0});
   }
 
   if (metricToSizeFiltered.size() == 1) {
@@ -244,8 +244,8 @@ void CaptionItem::generateSizeCaption(CaptionType captionType) {
 
   if (metricToSizeFiltered.size() < 2) {
     metricToSizeFiltered.clear();
-    metricToSizeFiltered.emplace_back(0., 1.);
-    metricToSizeFiltered.emplace_back(1., 1.);
+    metricToSizeFiltered.push_back({0., 1.});
+    metricToSizeFiltered.push_back({1., 1.});
     _captionGraphicsItem->generateSizeCaption(metricToSizeFiltered, "empty", 0., 1.);
     return;
   }

library/talipot-gui/src/GlyphRenderer.cpp
@@ -57,7 +57,7 @@ QPixmap GlyphRenderer::render(int glyphId) {
         // Create the glyph preview
         graph->getIntegerProperty("viewShape")->setNodeValue(node, glId);
         renderer->renderScene(false, true);
-        previews.emplace(std::make_pair(glId, QPixmap::fromImage(renderer->getImage())));
+        previews[glId] = QPixmap::fromImage(renderer->getImage());
       }
       renderer->clearScene(true);
     }
@@ -119,7 +119,7 @@ QPixmap EdgeExtremityGlyphRenderer::render(int glyphId) {
         // Create the glyph preview
         graph->getIntegerProperty("viewTgtAnchorShape")->setEdgeValue(e, glId);
         renderer->renderScene(true);
-        previews.emplace(std::make_pair(glId, QPixmap::fromImage(renderer->getImage())));
+        previews[glId] = QPixmap::fromImage(renderer->getImage());
       }
       renderer->clearScene(true);
     }

commit 3da8fcb2229c726b39f591be2064e7c4530775cc

talipot-ogl/GlCPULODCalculator: Fix segfault introduced in 92de3cd

library/talipot-ogl/src/GlCPULODCalculator.cpp
@@ -34,6 +34,7 @@ GlCPULODCalculator::~GlCPULODCalculator() {}
 void GlCPULODCalculator::beginNewCamera(Camera *camera) {
   // add a new layerLODUnit and set camera pointer
   layersLODVector.push_back(LayerLODUnit(camera));
+  currentLayerLODUnit = &layersLODVector.back();
 }
 
 void GlCPULODCalculator::addSimpleEntityBoundingBox(GlSimpleEntity *entity, const BoundingBox &bb) {